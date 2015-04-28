96ebabbd6bff0d41ffa8d176717825215f89258c
andrerocker <andre.souza@gmail.com>
2015-04-28 10:40:25 -0300

    add home action on jobs controller and map to root path

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,10 @@
 class JobsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+
+  def home
+    render text: "see -> github.com/andrerocker/atemporal :p"
+  end
 
   def index
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "jobs#home"
+
   resources :jobs, only: [:create, :index, :show] do
     member do
       get :callback
