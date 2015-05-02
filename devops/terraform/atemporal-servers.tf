6023c062c0a2cf5ce27bd5f66d18fda0adac74a2
andrerocker <andre.souza@gmail.com>
2015-05-02 20:10:55 -0300

    change server provision
    - move cloud-config to inside of terraform provision :(
    - up ec2 instance to c1.medium
    - add elb heartcheck (timout 2, interval 5)

--- devops/terraform/atemporal-servers.tf
+++ devops/terraform/atemporal-servers.tf
@@ -11,12 +11,20 @@ resource "aws_elb" "loadbalancer" {
     lb_protocol = "http"
   }
 
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 2
+    target = "HTTP:80/"
+    interval = 5
+  }
+
   instances = ["${aws_instance.servers.*.id}"]
 }
 
 resource "aws_instance" "servers" {
-  depends_on = ["aws_security_group.atemporal"]
-  instance_type = "t1.micro"
+  depends_on = ["aws_security_group.atemporal", "aws_instance.redis"]
+  instance_type = "c1.medium"
   ami = "ami-4df91b09"
   count = "${var.server_instances_count}"
   key_name = "${var.key_name}"
@@ -25,6 +33,34 @@ resource "aws_instance" "servers" {
   tags {
         Name = "Atemporal CoreOS ${count.index}"
   }
-  
-  user_data = "${replace(replace(file("../coreos/cloud-config.yml"), "DISCOVERY", var.cluster_discovery), "DOCKER_USERNAME", var.docker_username)}"
+
+  user_data = <<EOF
+#cloud-config
+
+write_files:
+  - path: /etc/atemporal
+    content: |
+      RAILS_ENV=production
+      REDIS_URL=redis://${aws_instance.redis.private_ip}:6379
+      DATABASE_URL=postgres://atemporal:atemporal@${aws_db_instance.database.endpoint}/atemporalpg
+      SECRET_KEY_BASE=c39362cda691f8394182f72c0c5b02bb6da54a9be6e374e948a7672db636de4e284a2e0f6dbe16b92f885d95e8075b2cfbcadc31dd2b4dd3e94eaac8711e1b3e
+
+coreos:
+  units:
+    - name: atemporal.service
+      command: start
+      content: |
+        [Unit]
+        Description=Atemporal
+        After=docker.service
+        Requires=docker.service
+        
+        [Service]
+        TimeoutStartSec=10m
+        ExecStartPre=-/usr/bin/docker kill atemporal
+        ExecStartPre=-/usr/bin/docker rm atemporal
+        ExecStartPre=/usr/bin/docker pull ${var.docker_username}/atemporal
+        ExecStart=/usr/bin/docker run --env-file=/etc/atemporal --name atemporal -p 80:80 ${var.docker_username}/atemporal /start
+        ExecStop=/usr/bin/docker stop atemporal
+EOF
 }
