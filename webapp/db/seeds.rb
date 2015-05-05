5e2a8dedb21e8cb4053a04d8af2d8da32a691409
andrerocker <andre.souza@gmail.com>
2015-05-04 23:24:57 -0300

    remove db seeds

--- webapp/db/seeds.rb
+++
@@ -1,3 +0,0 @@
-Job.create! image: "andrerocker/veryslowjob", time: Time.now
-Job.create! image: "andrerocker/brokecaptcha", time: Time.now, payload: "RETRY=3"
-Job.create! image: "andrerocker/seti", time: Time.now
