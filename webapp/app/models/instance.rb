473ec7f4c461eced843d7ea188da9f2dcaae5d50
andrerocker <andre.souza@gmail.com>
2015-05-05 23:48:41 -0300

    add instance base validations and specs

--- webapp/app/models/instance.rb
+++ webapp/app/models/instance.rb
@@ -1,3 +1,6 @@
 class Instance < ActiveRecord::Base
   belongs_to :job
+
+  validates :aws_id, presence: true
+  validates :job_id, presence: true
 end
--- webapp/spec/models/instance_spec.rb
+++ webapp/spec/models/instance_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Instance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "create a job" do
+    it "should create instance with default values" do
+      job = Job.create! image: "andrerocker/nginx", callback_server: "localhost"
+      current = subject.class.create(aws_id: "bacon", job_id: job.id) 
+
+      expect(current.aws_id).to eql "bacon"
+      expect(current.job_id).to eql job.id
+    end
+
+    it "should not create a instance without required fields " do
+      current = subject.class.create
+      expect(current.errors).to be_present
+
+      current = subject.class.create(aws_id: "bacon")
+      expect(current.errors).to be_present
+
+      current = subject.class.create(job_id: "bacon")
+      expect(current.errors).to be_present
+    end
+  end
 end
